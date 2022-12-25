422cbb63e0b86cdb604de89a12530d8efe2a4d88
TrueBloodDK <113499083+TrueBloodDK@users.noreply.github.com>
2022-12-25 17:41:04 +0500

    finalochka

--- AssemblyButinSiSharp/Program.cs
+++ AssemblyButinSiSharp/Program.cs
@@ -73,37 +73,83 @@ internal static class Program
         Console.WriteLine($"Введите числа A и B так чтобы A<B");
         int A = (int.Parse(Console.ReadLine()!));
         int B = (int.Parse(Console.ReadLine()!));
-        int sum = B-A+1; 
-        Console.WriteLine($"Кол-во цифр между ними включительно = {sum}");
-        while (B > A)
-        {
-            Console.WriteLine($"{A} + {B} = {A + B}");
-            A += B;
-            B--;
-            sum++;
-        }
-       Console.WriteLine($"Среднее арифметическое = {A/sum}");
+        int count = B-A+1;
+        int sum = 0;
+        Console.WriteLine($"Кол-во цифр между ними включительно = {count}");
+        while (B >= A) { sum += B; B--; }
+        Console.WriteLine($"Среднее арифметическое = {sum} / {count} = {sum/count}");
     }
+    
 /*12	Написать подпрограмму, вычисляющую НОК, и вычислить НОК для каждой пары чисел из трёх заданных.*/
+    static int NOD(int a, int b){
+        while (a != b){
+            if (a > b){
+                a -= b;
+            } else b -= a;
+        } return a; }
+
+    static int NOK(int a, int b) => Math.Abs(a * b)/NOD(a, b);
+    
     private static void work_4_1()
     {
+        Console.WriteLine($"Введите значение a"); 
+        var a = (int.Parse(Console.ReadLine()!));
+        Console.WriteLine($"Введите значение b"); 
+        var b = (int.Parse(Console.ReadLine()!));
+        Console.WriteLine($"Введите значение c"); 
+        var c = (int.Parse(Console.ReadLine()!));
+
+        Console.WriteLine($"НОК a и b = {NOK(a,b)}");
+        Console.WriteLine($"НОК b и c = {NOK(b,c)}");
+        Console.WriteLine($"НОК a и c = {NOK(a,c)}");
     }
     
 /*	Написать подпрограмму вычисления значения функции по формуле:
 Вычислить f(x_1 )+f(x_2).
 */
+    static int func1(int x) => x*x;
+    static int func2(int x) => (x*x)+(x*4)+(5);
+    static int func3(int x) => 4;
+
+    static int GetFunc(int x)
+    {
+        int f;
+        if (x >= 0 && x <= 2) f = func1(x);
+        else if (x > 2) f = func2(x);
+        else f = func3(x);
+        return f;
+    }
+
     private static void work_4_2()
     {
+        Console.WriteLine($"Введите значение x1"); 
+        var x1 = (int.Parse(Console.ReadLine()!));
+        Console.WriteLine($"Введите значение x2"); 
+        var x2 = (int.Parse(Console.ReadLine()!));
+
+        Console.WriteLine($"{GetFunc(x1)} + {GetFunc(x2)} = {GetFunc(x1) + GetFunc(x2)}");
     }
     
     /*2	Вычислить произведение четных элементов массива.*/
     private static void work_5_1()
     {
+        int[] array = new int[5] {2, 6, 7, 3, 3};
+        foreach (var t in array) Console.Write($"{t} ");
+        Console.WriteLine();
+
+        int mul = 1;
+        for (int i = 0; i < array.Length; i++)
+            if (array[i] % 2 == 0) mul *= array[i];
+        Console.WriteLine($"Произведение четных элементов = {mul}");
     }
     
     /*2	Записать строку a в обратном порядке.*/
     private static void work_5_2()
     {
+        Console.WriteLine($"Напишите строку которую хотите увидеть в обратном порядке"); 
+        var str = Console.ReadLine();
+        char[] reverse = str.Reverse().ToArray();
+        Console.WriteLine(reverse);
     }
 
 
@@ -122,9 +168,9 @@ internal static class Program
             [9] = work_5_1,
             [10] = work_5_2,
         };
-        Console.WriteLine("Какую лабу делаем? (1-8)");
-        int key = (int.Parse(Console.ReadLine()!));
-        if (works.ContainsKey(key)) works[key]();
-        else Console.WriteLine("Не такого задания");
-    }
+         Console.WriteLine("Какую лабу делаем? (1-8)");
+         int key = (int.Parse(Console.ReadLine()!));
+         if (works.ContainsKey(key)) works[key]();
+         else Console.WriteLine("Не такого задания");
+     }
 }
